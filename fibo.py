8200f14dbb758655379d3c921bf9023a7096af2e
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-22 16:13:33 -0400

    the fibonacci sequence works with the generator.

--- fibo.py
+++ fibo.py
@@ -1,9 +1,18 @@
-def list_nums():
-    list_num = [num for num in range(1,16)]
-    print(f"list of numbers = {list_num}")
+def fibo(num):
+    num1 = 0
+    num2 = 1
+    for i in range(num):
+        yield num1
+        temp = num1
+        num1 = num2
+        num2 = temp + num2
 
 def main():
-    list_nums()
-
+    fibo_seq = []
+    for n in fibo(16):
+        fibo_seq.append(n)
+    print(f"fibonacci sequence = {fibo_seq}")
+        
+        
 if __name__ =="__main__":
     main()
